8a996552612d88a70b9f9c9b64af50043df071b4
juber131 <jubayerahmad131@gmail.com>
2024-04-27 09:39:42 +0600

    add get operation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -34,6 +34,19 @@ async function run() {
 
     const travelCollection = client.db('travelDB').collection('travel');
     // const userCollection = client.db('coffeeDB').collection('user');
+
+
+    app.get('/tour', async (req, res) => {
+        const cursor = travelCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+    app.get('/tour/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await travelCollection.findOne(query);
+      res.send(result);
+  })
 
     app.post('/tour', async (req, res) => {
         const newtour = req.body;
