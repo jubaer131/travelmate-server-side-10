26df9cf2e2c6638f54bbe0e573026ed2f01f70c9
juber131 <jubayerahmad131@gmail.com>
2024-04-30 16:56:50 +0600

    add gitignore

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const port = process.env.PORT || 5000
 
 // midle ware
 app.use(cors())
-// app.use(cors({
-//   origin : ['http://localhost:5173/']
-// }))
+
 app.use(express.json())
 
 
@@ -40,6 +38,11 @@ app.get('/sport', async (req, res) => {
   const result = await cursor.toArray();
   res.send(result);
 })
+// app.get('/sport', async (req, res) => {
+//   const cursor = touristSportCollection.find();
+//   const result = await cursor.toArray();
+//   res.send(result);
+// })
 app.get('/sport/:id', async (req, res) => {
   const id = req.params.id;
   const query = { _id: new ObjectId(id) }
@@ -131,6 +134,7 @@ app.post('/tour', async (req, res) => {
   })
 
 // travel collection end
+
 
 
 
